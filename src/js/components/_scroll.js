3e60527a12679f568ac9b50d1329ba6e7ec65632
baxterandbailey-steve <128922950+baxterandbailey-steve@users.noreply.github.com>
2023-05-18 16:00:37 +0100

    JS to get and add banner height as margin-top to <main>

--- src/js/components/_scroll.js
+++ src/js/components/_scroll.js
@@ -1,6 +1,16 @@
 export function scroll() {
     /* swap logos and change header styling on scroll
------------------------------------------------------------------------------------------------------------------- */
+    --------------------------------------------------------------------------------------------------------- */
+    // get banner height
+    let banner = document.querySelector('.banner');
+    let bannerHeight = banner.offsetHeight;
+    console.log(bannerHeight);
+
+    // insert banner height margin-top on main
+    const main = document.querySelector('main');
+    main.style.marginTop = bannerHeight + 'px';
+
+    // object intersect attaching scrolled class to header
     const header = document.querySelector('header');
     const statement = document.querySelector('.statement');
     const statementOptions = {
