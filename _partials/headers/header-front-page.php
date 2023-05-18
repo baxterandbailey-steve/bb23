d2c3c21df744871e0c6e62a849dfdb33d498de98
baxterandbailey-steve <128922950+baxterandbailey-steve@users.noreply.github.com>
2023-05-18 12:29:09 +0100

    full-width class removed from banner, h1 container div discarded

--- _partials/headers/header-front-page.php
+++ _partials/headers/header-front-page.php
@@ -3,8 +3,6 @@
     <?php get_template_part('_partials/navbar'); ?>
 </header>
 
-<div class="banner full-width">
-    <div class="statement">
-        <h1>We are a multi award-winning creative agency. We help our clients make a positive impact in the world.</h1>
-    </div>
+<div class="banner">
+    <h1 class="statement">We are a multi award-winning creative agency. We help our clients make a positive impact in the world.</h1>
 </div>
