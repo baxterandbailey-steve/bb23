12d87655895b8570abaecb5f6a8556216ac51699
baxterandbailey-steve <128922950+baxterandbailey-steve@users.noreply.github.com>
2023-05-30 15:25:54 +0100

    Primary [titling] font updated to Cabinet sitewide

--- type.php
+++ type.php
@@ -14,7 +14,7 @@ get_template_part('_partials/headers/header', 'default'); ?>
         <!-- <p class="h1">Typography</p> -->
         <button class="toggle-bg">Toggle background colour</button>
     </section>
-    <section class=headings>
+    <!-- <section class=headings>
         <div class="qt-optimum">
             <h1>QT-Optimum</h1>
             <h1>Lorem ipsum dolar</h1>
@@ -33,7 +33,48 @@ get_template_part('_partials/headers/header', 'default'); ?>
             <h5>Lorem ipsum dolar</h5>
             <h6>Lorem ipsum dolar</h6>
         </div>
+    </section> -->
 
+    <section class="headings qt-optimum">
+        <h1>QT-Optimum</h1>
+        <span class="meta">h1 - 72px</span>
+        <h1>Lorem ipsum dolar</h1>
+        <span class="meta">h1 - 44px</span>
+        <h2>Lorem ipsum dolar</h2>
+        <span class="meta">h3 - 32px</span>
+        <h3>Lorem ipsum dolar</h3>
+        <span class="meta">h4 - 18px</span>
+        <h4>Lorem ipsum dolar</h4>
+        <span class="meta">p- 16px</span>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </section>
+
+    <section class="headings cabinet">
+        <h1>Cabinet Grotesk</h1>
+        <span class="meta">h1 - 72px</span>
+        <h1>Lorem ipsum dolar</h1>
+        <span class="meta">h2 - 44px</span>
+        <h2>Lorem ipsum dolar</h2>
+        <span class="meta">h3 - 32px</span>
+        <h3>Lorem ipsum dolar</h3>
+        <span class="meta">h4 - 18px</span>
+        <h4>Lorem ipsum dolar</h4>
+        <span class="meta">p- 16px</span>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </section>
+
+    <section class="headings satoshi">
+        <h1>Satoshi</h1>
+        <span class="meta">h1 - 72px</span>
+        <h1>Lorem ipsum dolar</h1>
+        <span class="meta">h2 - 44px</span>
+        <h2>Lorem ipsum dolar</h2>
+        <span class="meta">h3 - 32px</span>
+        <h3>Lorem ipsum dolar</h3>
+        <span class="meta">h4 - 18px</span>
+        <h4>Lorem ipsum dolar</h4>
+        <span class="meta">p - 16px</span>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
     </section>
 </main>
 
