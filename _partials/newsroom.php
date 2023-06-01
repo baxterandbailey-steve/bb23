89bff744ac2d633df037a06aaea5a8c3bfdf056b
baxterandbailey-steve <128922950+baxterandbailey-steve@users.noreply.github.com>
2023-06-01 11:40:00 +0100

    Basic newsroom partial set up and styled

---
+++ _partials/newsroom.php
@@ -0,0 +1,19 @@
+<section class="newsroom">
+    <div class="contain">
+        <h2 class="title">Newsroom</h2>
+        <ul class="news">
+            <li class="news-item">
+                <span class="category">Category</span>
+                <h3>Lorem ipsum dolar sit amet conse tincidunt facilisi facilisi.</h3>
+            </li>
+            <li class="news-item">
+                <span class="category">Category</span>
+                <h3>Lorem ipsum dolar sit amet consecutor malesuada urna diam feugiat sit venenatis at mi.</h3>
+            </li>
+            <li class="news-item">
+                <span class="category">Category</span>
+                <h3>Lorem ipsum dolar sit amet conse tincidunt facilisi variu.s</h3>
+            </li>
+        </ul>
+    </div>
+</section>
--- front-page.php
+++ front-page.php
@@ -60,79 +60,6 @@ get_template_part('_partials/headers/header', 'front-page'); ?>
         </div>
 
         <!-- row four -->
-        <div class="row row-four">
-            <figure class="lh-col" data-aos="fade-in">
-                <img src="/wp-content/uploads/2023/05/home-new-scientist.png" class="thumb">
-                <div class="details">
-                    <h2 class="client">New Scientist</h2>
-                    <span class="category">Category</span>
-                    <p class="description">We helped a vast US audience to believe in the power of new</p>
-                </div>
-            </figure>
-            <figure class="rh-col" data-aos="fade-in">
-                <img src="/wp-content/uploads/2023/05/home-saltus.png" class="thumb">
-                <div class="details">
-                    <h2 class="client">Saltus</h2>
-                    <span class="category">Category</span>
-                    <p class="description">Using distinct strategy & disarming creativity to show the human face of finance</p>
-                </div>
-            </figure>
-        </div>
-
-        <!-- row five -->
-        <div class="row row-five">
-            <figure class="lh-col" data-aos="fade-in">
-                <!-- <img src="/wp-content/uploads/2023/04/home-goldsmiths.png" class="thumb"> -->
-                <img src="/wp-content/uploads/2023/05/home-propellernet-3.png">
-                <div class="details">
-                    <h2 class="client">Client</h2>
-                    <span class="category">Category</span>
-                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
-                </div>
-            </figure>
-            <figure class="rh-col" data-aos="fade-in">
-                <img src="/wp-content/uploads/2023/05/home-nspcc.png" class="thumb">
-                <div class="details">
-                    <h2 class="client">NSPCC</h2>
-                    <span class="category">Category</span>
-                    <p class="description">Ensuring a crucial national lifeline is vital, visible and vibrant</p>
-                </div>
-            </figure>
-        </div>
-
-        <!-- row six -->
-        <div class="row row-six">
-            <figure class="rh-col" data-aos="fade-in">
-                <img src="/wp-content/uploads/2023/05/home-royal-mail-christmas-stamps.png" class="thumb">
-                <div class="details">
-                    <h2 class="client">Royal Mail Christmas Stamps</h2>
-                    <span class="category">Category</span>
-                    <p class="description">Putting a colourful contemporary twist on the Christmas story</p>
-                </div>
-            </figure>
-        </div>
-
-        <!-- row seven -->
-        <div class="row row-seven">
-            <figure class="lh-col" data-aos="fade-in">
-                <img src="/wp-content/uploads/2023/05/home-saltus.png" class="thumb">
-                <div class="details">
-                    <h2 class="client">Brickworks</h2>
-                    <span class="category">Category</span>
-                    <p class="description">Helping to revolutionise, reinvigorate and reinvent a sector, brick by brick</p>
-                </div>
-            </figure>
-            <figure class="rh-col" data-aos="fade-in">
-                <img src="/wp-content/uploads/2023/05/home-new-scientist-academy.png" class="thumb cover">
-                <div class="details">
-                    <h2 class="client">New Scientist Academy</h2>
-                    <span class="category">Category</span>
-                    <p class="description">Sharing the secrets of life, the universe and everything</p>
-                </div>
-            </figure>
-        </div>
-
-        <!-- row eight -->
         <div class="row row-eight">
             <figure class="lh-col" data-aos="fade-in">
                 <img src="/wp-content/uploads/2023/05/home-oxford-university-press.png" class="thumb">
@@ -152,7 +79,7 @@ get_template_part('_partials/headers/header', 'front-page'); ?>
             </figure>
         </div>
 
-        <!-- row nine -->
+        <!-- row five -->
         <div class="row row-nine">
             <figure class="lh-col" data-aos="fade-in">
                 <img src="/wp-content/uploads/2023/05/home-royal-mail-stones.png" class="thumb">
@@ -169,27 +96,11 @@ get_template_part('_partials/headers/header', 'front-page'); ?>
             </div>
         </div>
 
-        <!-- row ten -->
-        <div class="row row-ten">
-            <figure class="lh-col" data-aos="fade-in">
-                <img src="/wp-content/uploads/2023/05/home-brickworks-1.png">
-                <div class="details">
-                    <h2 class="client">Brickworks</h2>
-                    <span class="category">Category</span>
-                    <p class="description">Helping to revolutionise, reinvigorate and reinvent a sector, brick by brick</p>
-                </div>
-            </figure>
-            <figure class="rh-col" data-aos="fade-in">
-                <img src="/wp-content/uploads/2023/05/home-bb-showreel.png" class="thumb">
-                <div class="details">
-                    <h2 class="client">BB Show Reel 2021</h2>
-                    <span class="category">Category</span>
-                </div>
-            </figure>
-        </div>
+
     </div>
 
     <?php get_template_part('_partials/flx/blocks') ?>
+    <?php get_template_part('_partials/newsroom') ?>
 
 </main>
 <?php get_footer(); ?>
