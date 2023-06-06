3df36d9add55f903fbfb0f717d211a5fbc57126d
baxterandbailey-steve <128922950+baxterandbailey-steve@users.noreply.github.com>
2023-06-06 15:55:15 +0100

    Basic AOS fade-up data attributes added to homepage

--- front-page.php
+++ front-page.php
@@ -13,18 +13,18 @@ get_template_part('_partials/headers/header', 'front-page'); ?>
     <div class="grid grid-home contain">
         <!-- row one -->
         <div class="row row-one">
-            <figure class="col-1" data-aos="fade-in">
+            <figure class="col-1" data-aos="fade-up">
                 <h2 class="client">Propellernet</h2>
                 <div class="details">
                     <span class="category">Category</span>
                     <p class="description">Applying the rule of three to help a new category player launch, grow and win</p>
                 </div>
-                <img src="/wp-content/uploads/2023/05/home-propellernet-1.png" class="details thumb" data-aos="fade-in">
+                <img src="/wp-content/uploads/2023/05/home-propellernet-1.png" class="details thumb" data-aos="fade-up">
             </figure>
-            <figure class="col-2" data-aos="fade-in">
+            <figure class="col-2" data-aos="fade-up">
                 <img src="/wp-content/uploads/2023/05/home-propellernet-2.png" class="details thumb">
             </figure>
-            <figure class="col-3" data-aos="fade-in">
+            <figure class="col-3" data-aos="fade-up">
                 <img src="/wp-content/uploads/2023/05/home-propellernet-3.png">
             </figure>
         </div>
@@ -36,9 +36,9 @@ get_template_part('_partials/headers/header', 'front-page'); ?>
                 <span class="category">Category</span>
                 <blockquote>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor."</blockquote>
             </div>
-            <figure class="rh-col" data-aos="fade-left">
+            <figure class="rh-col" data-aos="fade-up">
                 <img src="/wp-content/uploads/2023/05/home-nspcc.png" class="thumb">
-                <div class="details" data-aos="fade-left">
+                <div class="details" data-aos="fade-up">
                     <h2 class="client">Threefold</h2>
                     <span class="category">Category</span>
                     <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</p>
@@ -50,7 +50,7 @@ get_template_part('_partials/headers/header', 'front-page'); ?>
         <div class="row row-three">
             <figure class="lh-col" data-aos="fade-up">
                 <img src="/wp-content/uploads/2023/05/home-brighton-and-hove-museums.png" class="thumb">
-                <div class="details" data-aos="fade-right">
+                <div class="details" data-aos="fade-up">
                     <h2 class="client">Brighton & Hove Museums</h2>
                     <span class="category">Category</span>
                     <p class="description">Lorem ipsum dolor sit amet. </p>
@@ -60,17 +60,17 @@ get_template_part('_partials/headers/header', 'front-page'); ?>
 
         <!-- row four -->
         <div class="row row-eight">
-            <figure class="lh-col" data-aos="fade-right">
+            <figure class="lh-col" data-aos="fade-up">
                 <img src="/wp-content/uploads/2023/05/home-oxford-university-press.png" class="thumb">
-                <div class="details" data-aos="fade-right">
+                <div class="details" data-aos="fade-up">
                     <h2 class="client">Oxford University Press</h2>
                     <span class="category">Category</span>
                     <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </div>
             </figure>
-            <figure class="rh-col" data-aos="fade-left">
+            <figure class="rh-col" data-aos="fade-up">
                 <img src="/wp-content/uploads/2023/05/home-lewes-fc.png">
-                <div class="details" data-aos="fade-left">
+                <div class="details" data-aos="fade-up">
                     <h2 class="client">Lewes FC</h2>
                     <span class="category">Category</span>
                     <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p>
@@ -80,7 +80,7 @@ get_template_part('_partials/headers/header', 'front-page'); ?>
 
         <!-- row five -->
         <div class="row row-nine">
-            <figure class="lh-col" data-aos="fade-right">
+            <figure class="lh-col" data-aos="fade-up">
                 <img src="/wp-content/uploads/2023/05/home-royal-mail-stones.png" class="thumb">
                 <div class="details" data-aos="fade-up>
                     <h2 class=" client">Royal Mail</h2>
@@ -88,7 +88,7 @@ get_template_part('_partials/headers/header', 'front-page'); ?>
                     <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing. </p>
                 </div>
             </figure>
-            <div class="quote rh-col" data-aos="fade-left">
+            <div class="quote rh-col" data-aos="fade-up">
                 <h2 class="client">Client</h2>
                 <span class="category">Category</span>
                 <blockquote>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</blockquote>
