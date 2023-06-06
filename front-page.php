5b7f343a79d5cb7ae2492de033abb22dbe6e892e
baxterandbailey-steve <128922950+baxterandbailey-steve@users.noreply.github.com>
2023-06-06 17:09:06 +0100

    Homepage titling styles updated, h2 added for selected work

--- front-page.php
+++ front-page.php
@@ -11,10 +11,11 @@ get_template_part('_partials/headers/header', 'front-page'); ?>
 
 <main>
     <div class="grid grid-home contain">
+        <h2 data-aos="fade-up">Selected Work</h2>
         <!-- row one -->
         <div class="row row-one">
             <figure class="col-1" data-aos="fade-up">
-                <h2 class="client">Propellernet</h2>
+                <h3 class="client">Propellernet</h3>
                 <div class="details">
                     <span class="category">Category</span>
                     <p class="description">Applying the rule of three to help a new category player launch, grow and win</p>
@@ -32,14 +33,14 @@ get_template_part('_partials/headers/header', 'front-page'); ?>
         <!-- row two -->
         <div class="row row-two">
             <div class="quote lh-col" data-aos="fade-up">
-                <h2 class="client">Client Name</h2>
+                <h3 class="client">Client Name</h3>
                 <span class="category">Category</span>
                 <blockquote>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor."</blockquote>
             </div>
             <figure class="rh-col" data-aos="fade-up">
                 <img src="/wp-content/uploads/2023/05/home-nspcc.png" class="thumb">
                 <div class="details" data-aos="fade-up">
-                    <h2 class="client">Threefold</h2>
+                    <h3 class="client">Threefold</h3>
                     <span class="category">Category</span>
                     <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</p>
                 </div>
@@ -51,7 +52,7 @@ get_template_part('_partials/headers/header', 'front-page'); ?>
             <figure class="lh-col" data-aos="fade-up">
                 <img src="/wp-content/uploads/2023/05/home-brighton-and-hove-museums.png" class="thumb">
                 <div class="details" data-aos="fade-up">
-                    <h2 class="client">Brighton & Hove Museums</h2>
+                    <h3 class="client">Brighton & Hove Museums</h3>
                     <span class="category">Category</span>
                     <p class="description">Lorem ipsum dolor sit amet. </p>
                 </div>
@@ -63,7 +64,7 @@ get_template_part('_partials/headers/header', 'front-page'); ?>
             <figure class="lh-col" data-aos="fade-up">
                 <img src="/wp-content/uploads/2023/05/home-oxford-university-press.png" class="thumb">
                 <div class="details" data-aos="fade-up">
-                    <h2 class="client">Oxford University Press</h2>
+                    <h3 class="client">Oxford University Press</h3>
                     <span class="category">Category</span>
                     <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </div>
@@ -71,7 +72,7 @@ get_template_part('_partials/headers/header', 'front-page'); ?>
             <figure class="rh-col" data-aos="fade-up">
                 <img src="/wp-content/uploads/2023/05/home-lewes-fc.png">
                 <div class="details" data-aos="fade-up">
-                    <h2 class="client">Lewes FC</h2>
+                    <h3 class="client">Lewes FC</h3>
                     <span class="category">Category</span>
                     <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p>
                 </div>
@@ -83,13 +84,13 @@ get_template_part('_partials/headers/header', 'front-page'); ?>
             <figure class="lh-col" data-aos="fade-up">
                 <img src="/wp-content/uploads/2023/05/home-royal-mail-stones.png" class="thumb">
                 <div class="details" data-aos="fade-up>
-                    <h2 class=" client">Royal Mail</h2>
+                    <h3 class=" client">Royal Mail</h3>
                     <span class="category">Category</span>
                     <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing. </p>
                 </div>
             </figure>
             <div class="quote rh-col" data-aos="fade-up">
-                <h2 class="client">Client</h2>
+                <h3 class="client">Client</h3>
                 <span class="category">Category</span>
                 <blockquote>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</blockquote>
             </div>
