75e12cffef10215447a3b7f4293a00866b799ce9
baxterandbailey-steve <128922950+baxterandbailey-steve@users.noreply.github.com>
2023-06-12 12:30:51 +0100

    Who we are template set-up

---
+++ who-we-are.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Template Name: Who we are
+ * 
+ * @package BBWP
+ */
+
+get_header();
+get_template_part('_partials/headers/header', 'default'); ?>
+
+<main></main>
+
+<?php get_footer(); ?>
