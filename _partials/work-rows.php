c60a7a1e976e9405faa29a6eb95b666e2982182c
baxterandbailey-steve <128922950+baxterandbailey-steve@users.noreply.github.com>
2023-06-14 12:57:37 +0100

    Basic semantics & styles added to work rows from hardcoded front-page.php

--- _partials/work-rows.php
+++ _partials/work-rows.php
@@ -1,78 +1,103 @@
-<section class="contain" style="color: white;">
-    <?php if (have_rows('work_rows')) : ?>
-        <?php while (have_rows('work_rows')) : the_row(); ?>
-            <div style="display: flex; justify-content: space-between; margin-bottom: 5rem;">
-                <!-- LH Col -->
-                <?php if (have_rows('lh_col')) : ?>
-                    <?php while (have_rows('lh_col')) : the_row(); ?>
-                        <!-- test for text or image -->
+<!-- start flexible-content -->
+<?php if (have_rows('work_rows')) : ?>
+    <?php while (have_rows('work_rows')) : the_row(); ?>
+        <div style="display: flex; justify-content: space-between; margin-bottom: 5rem;">
 
-                        <?php if (get_sub_field('choice') == 'text') : { ?>
-                                <div>
-                                    <?php if (have_rows('lh_text')) : ?>
-                                        <?php while (have_rows('lh_text')) : the_row(); ?>
-                                            <h3><?php the_sub_field('client'); ?></h3>
-                                            <p><?php the_sub_field('category'); ?></p>
-                                            <blockquopte><?php the_sub_field('quote'); ?></blockquopte>
-                                    <?php endwhile;
-                                    endif; ?>
-                                </div>
-                            <?php } ?>
+            <!--======= LH Col ================================================================================-->
+            <?php if (have_rows('lh_col')) : ?>
+                <?php while (have_rows('lh_col')) : the_row(); ?>
 
-                        <? else : ?>
+                    <!-- +++++++++++++++++++++ text ++++++++++++++++++++++++ -->
+                    <?php if (get_sub_field('choice') == 'text') : { ?>
 
-                            <div>
-                                <?php if (have_rows('lh_image')) : ?>
-                                    <?php while (have_rows('lh_image')) : the_row(); ?>
+
+                            <?php if (have_rows('lh_text')) : ?>
+                                <?php while (have_rows('lh_text')) : the_row(); ?>
+
+                                    <div class="quote lh-col" data-aos="fade-up">
+                                        <h3 class="client"><?php the_sub_field('client'); ?></h3>
+                                        <span class="category"><?php the_sub_field('category'); ?></span>
+                                        <blockquopte><?php the_sub_field('quote'); ?></blockquopte>
+                                    </div>
+
+                            <?php endwhile;
+                            endif; ?>
+
+                        <?php } ?>
+
+                    <? else : ?>
+
+                        <!-- +++++++++++++++++++++ image ++++++++++++++++++++++++ -->
+
+                        <?php if (have_rows('lh_image')) : ?>
+                            <?php while (have_rows('lh_image')) : the_row(); ?>
+
+                                <figure class="lh-col" data-aos="fade-up">
+                                    <img src="<?php the_sub_field('image'); ?>" style="margin-top:<?php the_sub_field('offset'); ?>">
+                                    <div class="details" data-aos="fade-up">
+                                        <h3 class="client"><?php the_sub_field('client'); ?></h3>
+                                        <span class="category"><?php the_sub_field('category'); ?></span>
+                                        <p class="description"><?php the_sub_field('description'); ?></p>
+                                    </div>
+                                </figure>
+
+                        <?php endwhile;
+                        endif; ?>
+
+
+                    <?php endif ?>
+
+                    <!-- close LH group loop-->
+            <?php endwhile;
+            endif; ?>
+
+            <!--======= RH Col ================================================================================-->
+            <?php if (have_rows('rh_col')) : ?>
+                <?php while (have_rows('rh_col')) : the_row(); ?>
+
+                    <!-- +++++++++++++++++++++ text ++++++++++++++++++++++++ -->
+                    <?php if (get_sub_field('choice') == 'text') : { ?>
+
+                            <?php if (have_rows('rh_text')) : ?>
+                                <?php while (have_rows('rh_text')) : the_row(); ?>
+
+                                    <div class="quote rh-col" data-aos="fade-up">
+                                        <h3 class="client"><?php the_sub_field('client'); ?></h3>
+                                        <span class="category"><?php the_sub_field('category'); ?></span>
+                                        <blockquopte><?php the_sub_field('quote'); ?></blockquopte>
+                                    </div>
+
+                            <?php endwhile;
+                            endif; ?>
+
+                        <?php } ?>
+
+                    <? else : ?>
+
+                        <!-- +++++++++++++++++++++ image ++++++++++++++++++++++++ -->
+                        <div class="row">
+                            <?php if (have_rows('rh_image')) : ?>
+                                <?php while (have_rows('rh_image')) : the_row(); ?>
+
+                                    <figure class="rh-col" data-aos="fade-up">
                                         <img src="<?php the_sub_field('image'); ?>" style="margin-top:<?php the_sub_field('offset'); ?>">
-                                        <h3><?php the_sub_field('client'); ?></h3>
-                                        <p><?php the_sub_field('category'); ?></p>
-                                        <blockquopte><?php the_sub_field('description'); ?></blockquopte>
-                                <?php endwhile;
-                                endif; ?>
-                            </div>
+                                        <div class="details" data-aos="fade-up">
+                                            <h3 class="client"><?php the_sub_field('client'); ?></h3>
+                                            <span class="category"><?php the_sub_field('category'); ?></span>
+                                            <p class="description"><?php the_sub_field('description'); ?></p>
+                                        </div>
+                                    </figure>
 
-                        <?php endif ?>
-                        <!-- close LH Col -->
-                <?php endwhile;
-                endif; ?>
+                            <?php endwhile;
+                            endif; ?>
+                        </div>
+                    <?php endif ?>
 
-                <!-- RH Col -->
-                <?php if (have_rows('rh_col')) : ?>
-                    <?php while (have_rows('rh_col')) : the_row(); ?>
-                        <!-- test for text or image -->
-                        <?php if (get_sub_field('choice') == 'text') : { ?>
+                    <!-- close RH group loop -->
+            <?php endwhile;
+            endif; ?>
+        </div>
 
-                                <div>
-                                    <?php if (have_rows('rh_text')) : ?>
-                                        <?php while (have_rows('rh_text')) : the_row(); ?>
-                                            <h3><?php the_sub_field('client'); ?></h3>
-                                            <p><?php the_sub_field('category'); ?></p>
-                                            <blockquopte><?php the_sub_field('quote'); ?></blockquopte>
-                                    <?php endwhile;
-                                    endif; ?>
-                                </div>
-                            <?php } ?>
-
-                        <? else : ?>
-
-                            <div>
-                                <?php if (have_rows('rh_image')) : ?>
-                                    <?php while (have_rows('rh_image')) : the_row(); ?>
-                                        <img src="<?php the_sub_field('image'); ?>" style="margin-top:<?php the_sub_field('offset'); ?>">
-                                        <h3><?php the_sub_field('client'); ?></h3>
-                                        <p><?php the_sub_field('category'); ?></p>
-                                        <blockquopte><?php the_sub_field('description'); ?></blockquopte>
-                                <?php endwhile;
-                                endif; ?>
-                            </div>
-                        <?php endif ?>
-
-                        <!-- close RH Col -->
-                <?php endwhile;
-                endif; ?>
-            </div>
-
-        <?php endwhile; ?><!-- close flexible content while -->
-    <?php endif; ?><!-- close flexible content if -->
-</section>
+        <!-- close flexible-content -->
+    <?php endwhile; ?>
+<?php endif; ?>
--- front-page.php
+++ front-page.php
@@ -30,74 +30,9 @@ get_template_part('_partials/headers/header', 'front-page'); ?>
             </figure>
         </div>
 
-        <!-- row two -->
-        <div class="row row-two">
-            <div class="quote lh-col" data-aos="fade-up">
-                <h3 class="client">Client Name</h3>
-                <span class="category">Category</span>
-                <blockquote>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor."</blockquote>
-            </div>
-            <figure class="rh-col" data-aos="fade-up">
-                <img src="/wp-content/uploads/2023/05/home-nspcc.png" class="thumb">
-                <div class="details" data-aos="fade-up">
-                    <h3 class="client">Threefold</h3>
-                    <span class="category">Category</span>
-                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</p>
-                </div>
-            </figure>
-        </div>
-
-        <!-- row three -->
-        <div class="row row-three">
-            <figure class="lh-col" data-aos="fade-up">
-                <img src="/wp-content/uploads/2023/05/home-brighton-and-hove-museums.png" class="thumb">
-                <div class="details" data-aos="fade-up">
-                    <h3 class="client">Brighton & Hove Museums</h3>
-                    <span class="category">Category</span>
-                    <p class="description">Lorem ipsum dolor sit amet. </p>
-                </div>
-            </figure>
-        </div>
-
-        <!-- row four -->
-        <div class="row row-eight">
-            <figure class="lh-col" data-aos="fade-up">
-                <img src="/wp-content/uploads/2023/05/home-oxford-university-press.png" class="thumb">
-                <div class="details" data-aos="fade-up">
-                    <h3 class="client">Oxford University Press</h3>
-                    <span class="category">Category</span>
-                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-            </figure>
-            <figure class="rh-col" data-aos="fade-up">
-                <img src="/wp-content/uploads/2023/05/home-lewes-fc.png">
-                <div class="details" data-aos="fade-up">
-                    <h3 class="client">Lewes FC</h3>
-                    <span class="category">Category</span>
-                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p>
-                </div>
-            </figure>
-        </div>
-
-        <!-- row five -->
-        <div class="row row-nine">
-            <figure class="lh-col" data-aos="fade-up">
-                <img src="/wp-content/uploads/2023/05/home-royal-mail-stones.png" class="thumb">
-                <div class="details" data-aos="fade-up>
-                    <h3 class=" client">Royal Mail</h3>
-                    <span class="category">Category</span>
-                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing. </p>
-                </div>
-            </figure>
-            <div class="quote rh-col" data-aos="fade-up">
-                <h3 class="client">Client</h3>
-                <span class="category">Category</span>
-                <blockquote>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</blockquote>
-            </div>
-        </div>
-
-
+        <?php get_template_part('_partials/work-rows') ?>
     </div>
+
 
     <?php get_template_part('_partials/flx/blocks') ?>
     <?php get_template_part('_partials/newsroom') ?>
