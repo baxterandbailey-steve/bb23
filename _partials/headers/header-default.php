daf7e937786a929ca7bae3a4734029c9372301f5
baxterandbailey-steve <128922950+baxterandbailey-steve@users.noreply.github.com>
2023-06-14 18:10:30 +0100

    Basic ACF banner and call set-up, tested and, commented out..

--- _partials/headers/header-default.php
+++ _partials/headers/header-default.php
@@ -1,4 +1,6 @@
 <header>
     <?php get_template_part('_partials/logo'); ?>
     <?php get_template_part('_partials/navbar'); ?>
+
+    <!-- <h1><?php the_field('statement'); ?></h1> -->
 </header>
--- _partials/headers/header-front-page.php
+++ _partials/headers/header-front-page.php
@@ -7,4 +7,6 @@
     <h1 class="statement sr-only">We are a multi award-winning creative agency. We help our clients make a positive impact in the world.</h1>
 
     <h1 class="statement" aria-hidden="true">We are a multi award-winning creative agency. We help our clients make a positive impact in the world.</h1>
+
+    <!-- <h1><?php the_field('statement'); ?></h1> -->
 </div>
