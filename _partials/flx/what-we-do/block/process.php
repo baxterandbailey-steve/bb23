6d8dc1a856768114c247ece6c8ee73394338f27a
baxterandbailey-steve <128922950+baxterandbailey-steve@users.noreply.github.com>
2023-06-19 15:07:00 +0100

    Process updated and styled to match design, pseudo elements to add'

--- _partials/flx/what-we-do/block/process.php
+++ _partials/flx/what-we-do/block/process.php
@@ -2,25 +2,35 @@
 $process_section_title = get_sub_field('section_title');
 ?>
 
-<section class="process">
-    <div class="inner grid contain">
-        <!-- <h2 class="title"><?php echo $process_section_title; ?></h2> -->
+<section class="processes">
+    <div class="contain">
+        <h2 class="title"><?php echo $process_section_title; ?></h2>
 
         <?php if (have_rows('process')) :
             while (have_rows('process')) : the_row();
+
                 $process_type = get_sub_field('type');
                 $process_title = get_sub_field('title');
                 $process_description = get_sub_field('description');
                 $process_image = get_sub_field('image');
         ?>
 
-                <div class="rich-text">
-                    <span class="meta"><?php echo $process_type; ?></span>
-                    <h3 class="title"><?php echo $process_title; ?></h3>
-                    <p class="description"><?php echo $process_description; ?></p>
+                <div class="process grid">
+                    <!--
+                        <style>
+                            .process:after {
+                                background-image: url('<?php echo $process_image; ?>');
+                            }
+                        </style>
+                    -->
+                    <div class="text">
+                        <span class="meta"><?php echo $process_type; ?></span>
+                        <h3 class="title"><?php echo $process_title; ?></h3>
+                        <p class="description"><?php echo $process_description; ?></p>
+                    </div>
                 </div>
 
             <?php endwhile; ?>
         <?php endif; ?>
-    </div>
+
 </section>
