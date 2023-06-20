6272ecd5eed1b3d5fed3ef2653c6e60e79a4f953
baxterandbailey-steve <128922950+baxterandbailey-steve@users.noreply.github.com>
2023-06-20 13:08:12 +0100

    Layout controls added to What we do text-image block

--- _partials/flx/what-we-do/block/text-image.php
+++ _partials/flx/what-we-do/block/text-image.php
@@ -2,9 +2,10 @@
 $text_image_title = get_sub_field('title');
 $text_image_text = get_sub_field('text');
 $text_image_image = get_sub_field('image');
+$text_image_layout = get_sub_field('layout');
 ?>
 
-<div class="text-image grid contain">
+<div class="text-image grid contain <?php echo $text_image_layout; ?>">
     <div class="text">
         <h2 class="title"><?php echo $text_image_title ?></h2>
         <?php echo $text_image_text ?>
