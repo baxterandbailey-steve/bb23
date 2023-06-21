8fb608cda15cec6f1970912a1a52311575338d52
baxterandbailey-steve <128922950+baxterandbailey-steve@users.noreply.github.com>
2023-06-21 13:11:35 +0100

    Basic media queries added for get in touch

--- _partials/page-sections/get-in-touch.php
+++ _partials/page-sections/get-in-touch.php
@@ -5,7 +5,7 @@ $newsletter_text = get_field('opt_newsletter_text', 'options');
 ?>
 
 <div class="get-in-touch">
-    <div class="inner grid contain">
+    <div class="container contain">
         <img src=" <?php echo $newsletter_image; ?>" class="image">
         <div class="text">
             <h2 class="title"><?php echo $newsletter_title;  ?></h2>
@@ -13,7 +13,7 @@ $newsletter_text = get_field('opt_newsletter_text', 'options');
 
             <form>
                 <label for="get_in_touch"></label>
-                <input type="email" id="get_in_touch" name="id=" get_in_touch" placeholder="Your email address" class="input-email">
+                <input type="email" id="get_in_touch" name="get_in_touch" placeholder="Your email address" class="input-email">
                 <label for="subscribe_button"></label>
                 <input type="submit" id="subscribe_button" name="subscribe_button" value="subscribe" class="input-subscribe">
             </form>
