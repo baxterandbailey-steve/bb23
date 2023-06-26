430cab7d3bfa59f73ab796b679c7c0e46bd43af5
baxterandbailey-steve <128922950+baxterandbailey-steve@users.noreply.github.com>
2023-06-26 18:05:14 +0100

    Temporary video blocks and calls added to work case-study

--- single-bbwp_cpt_work.php
+++ single-bbwp_cpt_work.php
@@ -15,8 +15,6 @@ $primary_image = get_field('primary_image');
 $secondary_image = get_field('secondary_image');
 ?>
 
-<!-- <?php get_template_part('_partials/headers/header', 'default'); ?> -->
-
 <main class="case-study">
     <section class="page-top contain">
         <span class="meta"><?php echo $service; ?></span>
@@ -33,7 +31,7 @@ $secondary_image = get_field('secondary_image');
             <div class="details">
                 <div class="sector">
                     <h3 class="title">Sector</h3>
-                    <p class="item"><?php echo $sector; ?></p>
+                    <p class="type"><?php echo $sector; ?></p>
                 </div>
 
                 <div class="what-we-did">
@@ -87,75 +85,129 @@ $secondary_image = get_field('secondary_image');
 
     <!-- Flexible content -->
     <div class="flexible-content">
-        <div class="contain">
 
-            <?php if (have_rows('flexible_content')) : ?>
-                <?php while (have_rows('flexible_content')) : the_row(); ?>
 
-                    <?php if (get_row_layout() == 'images') : ?>
-                        <?php if (get_sub_field('layout') == 'half') : { ?>
+        <?php if (have_rows('flexible_content')) : ?>
+            <?php while (have_rows('flexible_content')) : the_row(); ?>
+
+                <?php if (get_row_layout() == 'images') : ?>
+                    <?php if (get_sub_field('layout') == 'half') : { ?>
+                            <div class="single grid">
                                 <img src="<?php the_sub_field('single'); ?>" class="<?php the_sub_field('layout'); ?>">
-                            <?php } ?>
-
-                        <? elseif (get_sub_field('layout') == 'double') : ?>
-                            <?php if (have_rows('double')) : ?>
-                                <?php while (have_rows('double')) : the_row(); ?>
-
-                                    <div class="double grid">
-                                        <img src="<?php the_sub_field('lh_image'); ?>" class="lh-image">
-                                        <img src="<?php the_sub_field('rh_image'); ?>" class="rh-image">
-                                    </div>
-
-                            <?php endwhile;
-                            endif; ?>
-
-                            <? elseif (get_sub_field('layout') == 'full-width') : { ?>
-                                <img src="<?php the_sub_field('full_width'); ?>" class="<?php the_sub_field('layout'); ?>">
-                            <?php } ?>
-
-                        <? else : ?>
-                            <img src="<?php the_sub_field('full_bleed'); ?>" class="<?php the_sub_field('layout'); ?>">
-                            <?php ?>
-                        <?php endif ?>
-
-                    <?php elseif (get_row_layout() == 'text') : ?>
-                        <section class="text-row grid">
-                            <div class="lh-col">
-                                <h2><?php the_sub_field('title') ?></h2>
                             </div>
-                            <div class="rh-col">
-                                <p class="text"><?php the_sub_field('text') ?></p>
-                            </div>
-                        </section>
+                        <?php } ?>
 
-                    <?php elseif (get_row_layout() == 'image_text') : ?>
+                    <? elseif (get_sub_field('layout') == 'double') : ?>
+                        <?php if (have_rows('double')) : ?>
+                            <?php while (have_rows('double')) : the_row(); ?>
 
-                        <div class="image-text grid">
-                            <img src="<?php the_sub_field('image') ?>" class="image">
-                            <div class="text">
-                                <?php the_sub_field('text') ?>
-                            </div>
+                                <div class="double grid">
+                                    <img src="<?php the_sub_field('lh_image'); ?>" class="lh-image">
+                                    <img src="<?php the_sub_field('rh_image'); ?>" class="rh-image">
+                                </div>
+
+                        <?php endwhile;
+                        endif; ?>
+
+                        <? elseif (get_sub_field('layout') == 'full-width') : { ?>
+                            <img src="<?php the_sub_field('full_width'); ?>" class="<?php the_sub_field('layout'); ?>">
+                        <?php } ?>
+
+                    <? else : ?>
+                        <img src="<?php the_sub_field('full_bleed'); ?>" class="<?php the_sub_field('layout'); ?>">
+                        <?php ?>
+                    <?php endif ?>
+
+                <?php elseif (get_row_layout() == 'text') : ?>
+                    <section class="text-row grid">
+                        <div class="lh-col">
+                            <h2><?php the_sub_field('title') ?></h2>
+                        </div>
+                        <div class="rh-col">
+                            <?php the_sub_field('text') ?>
+                        </div>
+                    </section>
+
+                <?php elseif (get_row_layout() == 'image_text') : ?>
+
+                    <div class="image-text grid">
+                        <img src="<?php the_sub_field('image') ?>" class="image">
+                        <div class="text">
+                            <?php the_sub_field('text') ?>
+                        </div>
+                    </div>
+
+                <?php elseif (get_row_layout() == 'testimonial') : ?>
+
+                    <div class="testimonial grid">
+                        <div class="author-meta">
+                            <span class="author"><?php the_sub_field('author') ?></span>
+                            <span class="role"><?php the_sub_field('role') ?></span>
+                            <span class="organisation"><?php the_sub_field('organisation') ?></span>
                         </div>
 
-                    <?php elseif (get_row_layout() == 'testimonial') : ?>
+                        <p class="text"><?php the_sub_field('testimonial') ?></p>
+                    </div>
 
-                        <div class="testimonial grid">
-                            <div class="author-meta">
-                                <span class="author"><?php the_sub_field('author') ?></span>
-                                <span class="role"><?php the_sub_field('role') ?></span>
-                                <span class="organisation"><?php the_sub_field('organisation') ?></span>
-                            </div>
-
-                            <p class="text"><?php the_sub_field('testimonial') ?></p>
-                        </div>
-
-
+                <?php elseif (get_row_layout() == 'half_width') : ?>
+                    <?php if (get_sub_field('choice') == 'video') : ?>
+                        <video controls autoplay>
+                            <source src="<?php the_sub_field('video') ?>">
+                        </video>
+                    <?php else : ?>
+                        <img src="<?php the_sub_field('image'); ?>">
                     <?php endif; ?>
-            <?php endwhile; // close flex while
-            endif;  // close flex if 
-            ?>
 
-        </div><!--close contain -->
+
+                <?php elseif (get_row_layout() == 'full_width_two_cols') : ?>
+                    <?php if (have_rows('lh_col')) :
+                        while (have_rows('lh_col')) : the_row(); ?>
+
+                            <?php if (get_sub_field('choice') == 'video') : ?>
+                                <video controls autoplay>
+                                    <source src="<?php the_sub_field('video') ?>">
+                                </video>
+                            <?php else : ?>
+                                <img src="<?php the_sub_field('image'); ?>">
+                            <?php endif; ?>
+
+                    <?php endwhile;
+                    endif;
+                    ?>
+                    <?php if (have_rows('rh_col')) :
+                        while (have_rows('rh_col')) : the_row(); ?>
+
+                            <?php if (get_sub_field('choice') == 'video') : ?>
+                                <video controls autoplay>
+                                    <source src="<?php the_sub_field('video') ?>">
+                                </video>
+                            <?php else : ?>
+                                <img src="<?php the_sub_field('image'); ?>">
+                            <?php endif; ?>
+
+                    <?php endwhile;
+                    endif;
+                    ?>
+
+                <?php elseif (get_row_layout() == 'full_width') : ?>
+                    <video controls autoplay>
+                        <source src="<?php the_sub_field('video') ?>">
+                    </video>
+
+                <?php elseif (get_row_layout() == 'full_screen') : ?>
+                    <video controls autoplay>
+                        <source src="<?php the_sub_field('video') ?>">
+                    </video>
+
+
+
+
+                <?php endif; ?>
+        <?php endwhile; // close flex while
+        endif;  // close flex if 
+        ?>
+
+
     </div><!--close flexible-content -->
 
 
