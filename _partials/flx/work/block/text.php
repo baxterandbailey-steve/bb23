9182b988f542876f8e65bfca31435fb47c360817
baxterandbailey-steve <128922950+baxterandbailey-steve@users.noreply.github.com>
2023-06-28 12:10:08 +0100

    Work [case study] keyline fix applied

--- _partials/flx/work/block/text.php
+++ _partials/flx/work/block/text.php
@@ -1,8 +1,10 @@
-<section class="text-row grid">
-    <div class="lh-col">
-        <h2><?php the_sub_field('title') ?></h2>
-    </div>
-    <div class="rh-col">
-        <?php the_sub_field('text') ?>
+<section class="text-row">
+    <div class="container grid">
+        <div class="lh-col">
+            <h2><?php the_sub_field('title') ?></h2>
+        </div>
+        <div class="rh-col">
+            <?php the_sub_field('text') ?>
+        </div>
     </div>
 </section>
