54003b7be4ca55ddbf2c0e6057f891bfcf11c252
baxterandbailey-steve <128922950+baxterandbailey-steve@users.noreply.github.com>
2023-07-05 11:56:55 +0100

    Work landing page, two columns [right hand two thirds] blocks and calls set-up and working

--- _partials/flx/test/block/two-images.php
+++ _partials/flx/test/block/two-images.php
@@ -90,19 +90,78 @@
     <?php else : ?>
 
         <?php if (have_rows('two_thirds_right')) : ?>
-            <?php while (have_rows('two_thirds_right')) : the_row();
+            <?php while (have_rows('two_thirds_right')) : the_row(); ?>
 
-                $image_left = get_sub_field('image_one');
-                $image_right = get_sub_field('image_two');
-            ?>
 
-                <figure>
-                    <?php echo wp_get_attachment_image($image_left, 'full'); ?>
-                </figure>
 
-                <figure>
-                    <?php echo wp_get_attachment_image($image_right, 'full'); ?>
-                </figure>
+                <!-- ================ column one ================= -->
+                <?php
+                // check for and loop through column one 
+                if (have_rows('column_one')) : ?>
+                    <?php while (have_rows('column_one')) : the_row(); ?>
+
+                        <h2> Two thirds col one loop</h2>
+
+                        <?php
+                        // get data, set variables 
+                        $col_one_image = get_sub_field('image');
+                        $col_one_video = get_sub_field('video');
+                        ?>
+
+                        <?php
+                        // test choice value, if image display image
+                        if (get_sub_field('choice') == 'image') : ?>
+
+                            <figure>
+                                <?php echo wp_get_attachment_image($col_one_image, 'full'); ?>
+                            </figure>
+
+                        <?php // if not image, display video 
+                        else : ?>
+
+                            <video preload playsinline autoplay muted loop>
+                                <source src="<?php echo $col_one_video; ?>">
+                            </video>
+
+                        <?php endif; ?>
+
+                    <?php endwhile; ?>
+                <?php endif; ?>
+
+                <!-- ================ column two ================= -->
+                <?php
+                // check for and loop through column two
+                if (have_rows('col_two')) : ?>
+                    <?php while (have_rows('col_two')) : the_row(); ?>
+
+                        <h2> Two thirds col two loop</h2>
+
+                        <?php
+                        // get data, set variables 
+                        $col_two_image = get_sub_field('image');
+                        $col_two_video = get_sub_field('video');
+                        ?>
+
+                        <?php
+                        // test choice value, if image display image
+                        if (get_sub_field('choice') == 'image') : ?>
+
+                            <figure>
+                                <?php echo wp_get_attachment_image($col_two_image, 'full'); ?>
+                            </figure>
+
+                        <?php // if not image, display video 
+                        else : ?>
+
+                            <video preload playsinline autoplay muted loop>
+                                <source src="<?php echo $col_two_video; ?>">
+                            </video>
+
+                        <?php endif; ?>
+
+                    <?php endwhile; ?>
+                <?php endif; ?>
+
 
             <?php endwhile; ?>
         <?php endif; ?>
