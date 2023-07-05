441b947ba0ad1c84830ef481474f7ae65ae72421
baxterandbailey-steve <128922950+baxterandbailey-steve@users.noreply.github.com>
2023-07-05 10:15:05 +0100

    Image video choice added to work [landing page] full-width block

--- _partials/flx/test/block/full-width.php
+++ _partials/flx/test/block/full-width.php
@@ -7,11 +7,24 @@
  */
 
 $full_width_image = get_sub_field('image');
+$full_width_video = get_sub_field('video');
 ?>
 
 <div class="contain">
     <h1>full width..</h1>
-    <figure class>
-        <?php echo wp_get_attachment_image($full_width_image, 'full'); ?>
-    </figure>
+
+    <?php if (get_sub_field('choice') == 'image') : ?>
+
+        <figure class="image">
+            <?php echo wp_get_attachment_image($full_width_image, 'full'); ?>
+        </figure>
+
+    <?php else : ?>
+
+        <video preload playsinline autoplay muted loop>
+            <source src="<?php echo $full_width_video; ?>">
+        </video>
+
+    <?php endif; ?>
+
 </div>
