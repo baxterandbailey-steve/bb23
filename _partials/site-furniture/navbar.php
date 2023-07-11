7452b47b75df52d04b355c4afcbec0324cf080f1
baxterandbailey-steve <128922950+baxterandbailey-steve@users.noreply.github.com>
2023-07-11 17:35:44 +0100

    Desktop nav re-styled

--- _partials/site-furniture/navbar.php
+++ _partials/site-furniture/navbar.php
@@ -1,9 +1,10 @@
-<nav>
-	<?php wp_nav_menu(array(
-		'menu_class' => 'primary-nav',
-		'theme_location' => 'header-menu',
-		'container' => '',
-	)); ?>
-</nav>
-
-<button class="navicon"></button>
+<div class="navigation">
+	<nav>
+		<?php wp_nav_menu(array(
+			'menu_class' => 'primary-nav',
+			'theme_location' => 'header-menu',
+			'container' => '',
+		)); ?>
+	</nav>
+	<button class="navicon"></button>
+</div>
