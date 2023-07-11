632cf8eed43deef1f51285b57d47066ae707a920
baxterandbailey-steve <128922950+baxterandbailey-steve@users.noreply.github.com>
2023-07-11 13:31:53 +0100

    Footer: mark-up and variables updated, header, lh and rh cols styled

--- footer.php
+++ footer.php
@@ -1,86 +1,96 @@
-		<?php
+	<?php
 
-		/**
-		 * Site footer
-		 *
-		 * @package BBWP
-		 */
-
-		$statement = get_field('statement', 'options');
-
-		$address = get_field('address', 'options');
-		$telephone_number = get_field('telephone_number', 'options');
-		$new_business = get_field('new_business_email', 'options');
-		$press = get_field('press_events_email', 'options');
-		$general_enquiries = get_field('general_enquiries_email', 'options');
-		$jobs = get_field('jobs_email', 'options');
-		$join_us = get_field('join_us', 'options');
+	/**
+	 * Site footer
+	 *
+	 * @package BBWP
+	 */
 
 
-		$instagram = get_field('instagram', 'options');
-		$twitter = get_field('twitter', 'options');
-		$pinterest = get_field('pinterest', 'options');
-		$linkedin = get_field('linkedin', 'options');
-		$behance = get_field('behance', 'options');
-		$the_dots = get_field('the_dots' . 'options');
-		$creative_pool = get_field('creative_pool', 'options');
+	$heading_line_one = get_field('opt_footer_heading_line_one', 'options');
+	$heading_line_two = get_field('opt_footer_heading_line_two', 'options');
+	$statement = get_field('opt_footer_statement', 'options');
 
-		?>
+	$address = get_field('opt_footer_address', 'options');
+	$telephone_number = get_field('opt_footer_tel_number', 'options');
+	$new_business = get_field('opt_footer_new_biz_email', 'options');
+	$press = get_field('opt_footer_press_email', 'options');
+	$general_enquiries = get_field('opt_footer_enquiries_email', 'options');
+	$join_us = get_field('opt_footer_join_us', 'options');
+	$join_us_link = get_field('opt_footer_join_us_link', 'options');
 
-		<footer role="contentinfo">
-			<div class="contain">
-				<div class="footer-main">
-					<div class="lh-col">
-						<span class="meta">Interested?</span>
-						<h2 class="title">We would love to hear from you</h2>
-						<p class="text"><?php echo $statement ?></p>
-					</div>
-					<div class="rh-col">
-						<div class="contact">
-							<h3>New Business</h3>
-							<span><?php echo $new_business ?></span>
-							<h3>Press and Events</h3>
-							<span><?php echo $press ?></span>
-							<h3>General Enquiries</h3>
-							<span><a href="tel: <?php echo $telephone_number ?>" class="phone"><?php echo $telephone_number ?></a></span><br>
-							<span><?php echo $general_enquiries ?></span>
-							<h3>Join Us</h3>
-							<p><?php echo $join_us; ?></p>
-							<span><?php echo $jobs ?></span><br>
+	$instagram = get_field('opt_instagram', 'options');
+	$twitter = get_field('opt_twitter', 'options');
+	$pinterest = get_field('opt_pinterest', 'options');
+	$linkedin = get_field('opt_linkedin', 'options');
+	$behance = get_field('opt_behance', 'options');
+	$the_dots = get_field('opt_the_dots' . 'options');
+	$creative_pool = get_field('opt_creative_pool', 'options');
 
-							<!--
-								<p>
-									<address>
-										<?php echo $address ?>
-									</address>
-								</p>
-							-->
-						</div>
-						<div class="social">
-							<h3>Follow Us</h3>
-							<ul class="socials">
-								<li>Instagram</li>
-								<li>Twitter</li>
-								<li>Pinterest</li>
-								<li>Linkedin</li>
-								<li>Behance</li>
-								<li>The Dots</li>
-								<li>Creative Pool</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-				<div class="footer-supplementary">
-					<small>&#169; Baxter & Bailey <?php echo date("Y") ?></small>
+	?>
+
+	<footer>
+		<div class="inner">
+			<h2 class="section-title">
+				<?php echo $heading_line_one; ?><br>
+				<?php echo $heading_line_two; ?>
+			</h2>
+
+			<div class="lh-col">
+				<p class="text"><?php echo $statement; ?></p>
+				<small>&#169; Baxter & Bailey <?php echo date("Y") ?></small>
+			</div>
+
+			<div class="rh-col">
+				<ul class="contact">
+					<li>
+						<span class="meta">New Business</span>
+						<a class="link"><?php echo $new_business; ?></a>
+					</li>
+					<li>
+						<span class="meta">Press and Events</span>
+						<a class="link"><?php echo $press; ?></a>
+					</li>
+					<li>
+						<span class="meta">General Enquiries</span>
+						<a href="tel: <?php echo $telephone_number; ?>" class="link"><?php echo $telephone_number; ?></a>
+						<a class="link"><?php echo $general_enquiries; ?></a>
+					</li>
+					<li>
+						<span class="meta">Join Us</span>
+						<p class="join-us-statement"><?php echo $join_us; ?></p>
+						<a class="link"><?php echo $join_us_link; ?></a>
+					</li>
+				</ul>
+
+				<div class="socials-and-address">
+					<h3>Follow Us</h3>
+					<ul class="socials">
+						<li><a href="<?php echo $instagram; ?>" class="link">Instagram</a></li>
+						<li><a href="<?php echo $twitter; ?>" class="link">Twitter</a></li>
+						<li><a href="<?php echo $pinterest; ?>" class="link">Pinterest</a></li>
+						<li><a href="<?php echo $linkedin; ?>" class="link">Linkedin</a></li>
+						<li><a href="<?php echo $behance; ?>" class="link">Behance</a></li>
+						<li><a href="<?php echo $the_dots; ?>" class="link">The Dots</a></li>
+						<li><a href="<?php echo $creative_pool; ?>" class="link">Creative Pool</a></li>
+					</ul>
+					<address>
+						<h3>Find us here</h3>
+						<?php echo $address; ?>
+					</address>
 				</div>
 			</div>
-		</footer>
 
-		<script src="https://cdn.jsdelivr.net/npm/gsap@3/dist/gsap.min.js"></script>
-		<script src="https://cdn.jsdelivr.net/npm/gsap@3/dist/ScrollTrigger.min.js"></script>
-		<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-		<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-		<?php wp_footer(); ?>
-		</body>
+		</div>
+	</footer>
 
-		</html>
+	<script src="https://cdn.jsdelivr.net/npm/gsap@3/dist/gsap.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/gsap@3/dist/ScrollTrigger.min.js"></script>
+	<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+	<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+
+	<?php wp_footer(); ?>
+
+	</body>
+
+	</html>
