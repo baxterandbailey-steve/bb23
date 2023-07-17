e1c3e4d983795cabf617acc0d8cf825ffdc80033
baxterandbailey-steve <128922950+baxterandbailey-steve@users.noreply.github.com>
2023-07-17 16:02:30 +0100

    Case study two-cols [server-side] display bug fixed

--- _partials/flx/case-studies/block/full-width-two-cols.php
+++ _partials/flx/case-studies/block/full-width-two-cols.php
@@ -9,33 +9,34 @@
 ?>
 
 <div class="two-cols-media">
-    <?php if (have_rows('lh_col')) : ?>
-
-        <? while (have_rows('lh_col')) : the_row();
+    <?php if (have_rows('lh_col')) :
+        while (have_rows('lh_col')) : the_row();
             $lh_video = get_sub_field('video');
             $lh_image = get_sub_field('image');
-        ?>
+    ?>
 
             <?php if (get_sub_field('choice') == 'video') : ?>
+
                 <video preload playsinline autoplay muted loop class="lh-video">
                     <source src="<?php echo $lh_video ?>" type="video/mp4">
                 </video>
 
             <?php else : ?>
+
                 <figure class="lh-image">
                     <?php echo wp_get_attachment_image($lh_image, 'full'); ?>
                 </figure>
+
             <?php endif; ?>
 
-        <? endwhile; ?>
-    <? endif; ?>
+    <?php endwhile;
+    endif; ?>
 
-
-    <?php if (have_rows('rh_col')) : ?>
-        <? while (have_rows('rh_col')) : the_row();
+    <?php if (have_rows('rh_col')) :
+        while (have_rows('rh_col')) : the_row();
             $rh_video = get_sub_field('video');
             $rh_image = get_sub_field('image');
-        ?>
+    ?>
 
             <?php if (get_sub_field('choice') == 'video') : ?>
                 <video preload playsinline autoplay muted loop class="rh-video">
@@ -48,7 +49,7 @@
                 </figure>
             <?php endif; ?>
 
-        <? endwhile; ?>
-    <? endif; ?>
+    <?php endwhile;
+    endif; ?>
 
 </div>
