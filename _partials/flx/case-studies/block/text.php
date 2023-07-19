7271d20b12c453ef649b18d503fd93b006f43e4a
baxterandbailey-steve <128922950+baxterandbailey-steve@users.noreply.github.com>
2023-07-19 11:46:54 +0100

    Case study: Basic stats repeater added to text block

--- _partials/flx/case-studies/block/text.php
+++ _partials/flx/case-studies/block/text.php
@@ -10,11 +10,28 @@ $sector_info_title  = get_sub_field('title');
 $sector_info_text   = get_sub_field('text');
 ?>
 
-<section class="rich-text">
-    <div class="container__centered">
+
+<div class="text-block">
+
+    <div class="rich-text">
         <h2 class="title"><?php echo $sector_info_title; ?></h2>
         <div class="text">
             <?php echo $sector_info_text; ?>
         </div>
     </div>
-</section>
+
+    <ul class="stats">
+        <?php if (have_rows('stats')) :
+            while (have_rows('stats')) : the_row();
+                $stat = get_sub_field('stat');
+                $description = get_sub_field('description');
+        ?>
+                <li>
+                    <span class="stat"><?php echo $stat; ?></span>
+                    <span class="description"><?php echo $description; ?></span>
+                </li>
+        <?php endwhile;
+        endif; ?>
+    </ul>
+
+</div>
