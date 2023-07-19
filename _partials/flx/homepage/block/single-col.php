4c59e6b4efb1b650d971589852c4c430a35c0702
baxterandbailey-steve <128922950+baxterandbailey-steve@users.noreply.github.com>
2023-07-19 15:47:57 +0100

    Homepage flexible content blocks: video added to single-col

--- _partials/flx/homepage/block/single-col.php
+++ _partials/flx/homepage/block/single-col.php
@@ -6,7 +6,9 @@
  * @package BBWP
  */
 
+// $single_col_choice = get_sub_field('choice');
 $single_col_image = get_sub_field('image');
+$single_col_video = get_sub_field('video');
 $single_col_width = get_sub_field('width');
 $single_col_client = get_sub_field('client');
 $single_col_category = get_sub_field('category');
@@ -15,14 +17,25 @@ $single_col_link = get_sub_field('link');
 ?>
 
 <div class="row single-col">
-    <figure style="grid-column:<?php echo $single_col_width ?>" data-aos="fade-up">
-        <a href="<?php echo $single_col_link; ?>">
+
+    <?php if (get_sub_field('choice') == 'image') : ?>
+
+        <figure style="grid-column:<?php echo $single_col_width ?>" data-aos="fade-up">
             <img src="<?php echo wp_get_attachment_image($single_col_image, 'full'); ?>">
-        </a>
-        <div class="details" data-aos="fade-up">
-            <h3 class="client"><?php echo $single_col_client; ?></h3>
-            <span class="category"><?php echo $single_col_category; ?></span>
-            <p class="description"><?php echo $single_col_description; ?></p>
-        </div>
-    </figure>
+        </figure>
+
+    <?php else : ?>
+
+        <video preload playsinline autoplay muted loop style="grid-column: <?php echo $single_col_width; ?>;">
+            <source src=" <?php echo $single_col_video; ?>">
+        </video>
+
+    <?php endif; ?>
+
+    <div class="details" data-aos="fade-up">
+        <h3 class="client"> <a href="<?php echo $single_col_link; ?>"><?php echo $single_col_client; ?></a></h3>
+        <span class="category"><?php echo $single_col_category; ?></span>
+        <p class="description"><?php echo $single_col_description; ?></p>
+    </div>
+
 </div>
