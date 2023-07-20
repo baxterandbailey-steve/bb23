0854c69e37f53aad6c0ccb3367494464298f80d7
baxterandbailey-steve <128922950+baxterandbailey-steve@users.noreply.github.com>
2023-07-20 13:28:31 +0100

    Homepage: video added to three cols

--- _partials/flx/homepage/block/three-cols.php
+++ _partials/flx/homepage/block/three-cols.php
@@ -7,6 +7,7 @@
  */
 
 $col_1_image = get_sub_field('col_1_image');
+$col_1_video = get_sub_field('col_1_video');
 $col_1_client = get_sub_field('col_1_client');
 $col_1_category = get_sub_field('col_1_category');
 $col_1_description = get_sub_field('col_1_description');
@@ -15,14 +16,25 @@ $col_3_image = get_sub_field('col_3_image');
 ?>
 
 <div class="row three-cols">
-    <figure class="col-1" data-aos="fade-up">
-        <h3 class="client"><?php echo $col_1_client; ?></h3>
-        <div class="details">
-            <span class="category"><?php echo $col_1_category; ?></span>
-            <p class="description"><?php echo $col_1_description; ?></p>
-        </div>
-        <img src="<?php echo wp_get_attachment_image($col_1_image, 'full'); ?>" class="details thumb" data-aos="fade-up">
-    </figure>
+
+    <?php if (get_sub_field('choice') == 'image') : ?>
+
+        <figure class="col-1" data-aos="fade-up">
+            <h3 class="client"><?php echo $col_1_client; ?></h3>
+            <div class="details">
+                <span class="category"><?php echo $col_1_category; ?></span>
+                <p class="description"><?php echo $col_1_description; ?></p>
+            </div>
+            <img src="<?php echo wp_get_attachment_image($col_1_image, 'full'); ?>" class="details thumb" data-aos="fade-up">
+        </figure>
+
+    <?php else : ?>
+
+        <video preload playsinline autoplay muted loop class="col-1">
+            <source src=" <?php echo $col_1_video; ?>">
+        </video>
+
+    <?php endif; ?>
 
     <figure class="col-3" data-aos="fade-up">
         <img src="<?php echo wp_get_attachment_image($col_3_image, 'full'); ?>">
