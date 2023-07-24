12b49fb171087ef820fb6e7709b0163c6411ebfb
baxterandbailey-steve <128922950+baxterandbailey-steve@users.noreply.github.com>
2023-07-24 17:57:53 +0100

    Team Member [overlay] : Favourite projects calls added, overlay styled and re-factored

--- single-bbwp_cpt_team.php
+++ single-bbwp_cpt_team.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Team CPT: Single
+ * Team: team member page
  *
  * @package BBWP
  */
@@ -9,13 +9,56 @@
 $team_title = get_field('team_title');
 $team_bio = get_field('team_bio');
 $team_linkedin = get_field('team_linkedin');
+$team_favourite = get_field('favourite_project');
+$favourite_description = get_field('favourite_description');
 
+get_header();
 ?>
 
-<div class="team-member">
-    <code>this is single-team.php</code>
+<main class="team-member">
+    <div class="container">
 
-    <?php the_title(); ?>
-    <?php echo $team_title; ?>
-    <?php echo $team_bio; ?>
-</div>
+        <div class="profile-top">
+            <button class=close></button>
+        </div>
+
+        <div class="profile">
+            <figure class="image">
+                <?php the_post_thumbnail(); ?>
+            </figure>
+
+            <div class="description">
+                <h1 class="team-member-name"><?php the_title(); ?></h1>
+                <span class="role"><?php echo $team_title; ?></span>
+                <?php echo $team_bio; ?>
+
+                <?php if (get_field('team_linkedin')) : ?>
+                    <a href="<?php echo $team_linkedin; ?>" class="linkedin">linkedin</a>
+                <?php endif ?>
+            </div>
+        </div>
+
+        <div class="project">
+            <?php foreach ($team_favourite as $post) :
+                setup_postdata($post); ?>
+
+                <figure>
+                    <a href="<?php the_permalink(); ?>">
+                        <?php the_post_thumbnail(); ?>
+                    </a>
+                </figure>
+
+                <div class="project-info">
+                    <h2 class="favourite">Favourite Project</h2>
+                    <span class="project-title"><?php the_title(); ?></span>
+                    <span class="category">Category</span>
+                    <p><?php echo $favourite_description; ?></p>
+                    <a href="<?php the_permalink(); ?>" class="link">View Case Study</a>
+                </div>
+            <?php endforeach; ?>
+
+            <?php wp_reset_postdata(); ?>
+        </div>
+
+    </div>
+</main>
